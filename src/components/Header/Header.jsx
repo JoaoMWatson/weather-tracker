4e85138ca337e0ccf3542b305a9e646b6a0cb0ae
JoaoMWatson <joaopedrowatson@hotmail.com>
2020-05-29 22:23:57 -0300

    Header pronto, search no inicio

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Header.css';
+
+export default function Header() {
+    return(
+        <div className="Header"> 
+            <h1>
+                São Paulo - Jardim Taquaral
+            </h1>
+        </div>
+    );
+}
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Search.css";
+
+export default function Search() {
+  return (
+    <input
+      class="Search"
+      placeholder="Procurar..."
+      type="text"
+      name="city"
+    />
+  );
+}

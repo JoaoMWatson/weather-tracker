0508b1645ffa91261663b6be1e9a7986b718b05c
JoaoMWatson <joaopedrowatson@hotmail.com>
2020-06-08 21:04:30 -0300

    weatherInfo component terminado

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
 
 import "./App.css";
+import WeatherInfo from "./components/WetherInfo/WeatherInfo";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
+        <WeatherInfo/>
       </div>
       <div className="footer">
         <Search />
--- src/components/WetherInfo/WeatherInfo.jsx
+++ src/components/WetherInfo/WeatherInfo.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
-import './WeatherInfo.css';
+import "./WeatherInfo.css";
 
-export default function WeatherInfo(){
-    return(
-        <div className="">
-
+class WeatherInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="weather-info">
+        <div className="humidade paragrafo">
+          <p>Humidade: </p>
         </div>
+        <hr className="linha top-hr"></hr>
+        <hr className="linha bottom-hr"></hr>
+        <div className="vento paragrafo">
+          <p>Velocidade: </p>
+          <p>Vento: </p>
+          <p>Direcão: </p>
+        </div>
+        <hr className="linha top-hr"></hr>
+        <hr className="linha bottom-hr"></hr>
+        <div className="sol paragrafo">
+          <p>Nascer do sol: </p>
+          <p>Por do sol: </p>
+        </div>
+      </div>
     );
+  }
 }
+
+export default WeatherInfo;
